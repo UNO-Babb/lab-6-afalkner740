48943c8a38f02c2d66781abf8bb6803fec8eb1d7
afalkner740 <afalknerpersonal@gmail.com>
2025-10-05 23:26:55 +0000

    Done with lab.

--- DiceRoll.py
+++ DiceRoll.py
@@ -6,13 +6,23 @@ import random
 
 def main():
   #Create an empty list with possible roll values
-  rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
+  rolls = [0]*13
+  totalsum = 0
   #Create two dice values ranging from 1 - 6 each
-  
-  #find the sum total of the two dice
-  
-  #print statictics for dice rolls
+  for r in range(100):
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    rolltotal = dice1 + dice2
+    rolls[rolltotal] += 1
+    totalsum += rolltotal
 
+  #find the sum total of the two dice
+  print("The Sum of the two die is:" ,totalsum)
+  #print statictics for dice rolls
+  dice = 1 
+  for count in rolls:
+    print ("Dice roll",dice, ": occurred", count, "times!")
+    dice = dice +1
 
 if __name__ == '__main__':
   main()
--- WordCount.py
+++ WordCount.py
@@ -4,11 +4,54 @@
 #Assignment:
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
-  
-  for line in textFile:
-    print(line)
-  
+  text = input("Would you like to use gettysberg or fish txt? (1 or 2): ")
+  if text == "1":
+    filename = "gettysberg.txt"
+    with open (filename, 'r') as textFile:
+      linecount = 0
+      wordcount = 0
+      lettercount = 0
+
+      for line in textFile:
+        linecount += 1
+        words = line.split()
+
+        for w in words:
+          wordcount += 1
+          w = w.lower()
+          w = w.replace("," ,"")
+          w = w.replace(".","")
+          for char in w:
+            if char.isalpha():
+              lettercount += 1
+
+  elif text == "2":
+    filename = "fish.txt"
+    with open (filename, 'r') as textFile:
+      linecount = 0
+      wordcount = 0
+      lettercount = 0
+
+      for line in textFile:
+        linecount += 1
+        words = line.split()
+
+        for w in words:
+          wordcount += 1
+          w = w.lower()
+          w = w.replace(",", "")
+          w = w.replace(".","")
+          for char in w:
+            if char.isalpha():
+              lettercount += 1
+  else:
+    print("Invalid choice!")
+    return
+      
+    #print(line)
+  print("Lines:", linecount)
+  print("Words:", wordcount)
+  print("Letters:", lettercount)
 
 if __name__ == '__main__':
   main()
